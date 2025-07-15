58a4cbe1ef076c7b7087fe1d3427d3ca4080b395
Zandrelius <zandreliusakain1@gmail.com>
2025-07-15 21:43:16 +0300

    changes 1

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL:"https://blogit-be-1.onrender.com/api",
+   baseURL:"https://blogit-be-1.onrender.com",
     withCredentials: true
 });
 
+// const api = axios.post('https://blogit-be-1.onrender.com/api/auth/register', data, {
+//   withCredentials: true
+// });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
